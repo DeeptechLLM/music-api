b0963d6cfcc1654ab02c4d51d7c40fd406deebf7
Bapuka <bapuka@gmail.com>
2024-05-24 14:15:06 +0800

    changes

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import joblib
 
 class ProdConfig():
     # load model with configs 
-    track_file = 'data/dat_tracks_without_ardiin_and_zohioliin_and_karaoke.jlb'
-    genre_file = 'data/dat_genres_without_ardiin_and_zohioliin_and_karaoke.jlb'
-    emotion_file = 'data/dat_emotions_without_ardiin_and_zohioliin_and_karaoke.jlb'
-    model_file = 'data/mod_tracks_without_ardiin_and_zohioliin_and_karaoke.jlb'   
+    track_file = 'data/v2/dat_tracks_without_karaoke.jlb'
+    genre_file = 'data/v2/dat_genres7_without_karaoke.jlb'
+    emotion_file = 'data/v2/dat_emotions3_without_karaoke.jlb'
+    model_file = 'data/v2/mod_tracks_without_karaoke.jlb'   
     
     # Only tracks and model for Zohioliin genre
     track_zohioliin = 'data/dat_tracks_zohioliin_without_karaoke.jlb'
@@ -30,4 +30,40 @@ class ProdConfig():
     MODEL = joblib.load(model_file)
     MODEL_ZOHIOLIIN = joblib.load(model_zohioliin)
     MODEL_ARDIIN = joblib.load(model_ardiin)
+    
+    GENRE_MAP_BY_NAME = {
+        "hiphop_rap": "hiphop",
+        "trap": "hiphop",
+        "jazz": "jazz",
+        "instrumentals": "pop",
+        "movie_songs": "pop",
+        "pop": "pop",
+        "electronics": "pop",
+        "r_and_b": "pop",
+        "reggae": "pop",
+        "pop_opera": "pop",
+        "country": "pop",
+        "rock_alternative": "rock",
+        "metal": "rock",
+        "ardiin": "mongol_folk",
+        "mongol_country": "mongol_country",
+        "zohioliin": "mongol_country",
+        "long": "mongol_country",
+        "children": "kids",
+        "ethnic": "ethnic",
+        "classic": "classic",
+        "orchestral": "classic",
+        "kids": "kids"
+    }
+    GENRE_MAP = {
+        "hiphop": ["hiphop", "rap", "triphop"],
+        "jazz": ["jazz", "acidjazz","improvisation","jazzfunk","jazzfusion"],
+        "pop": ["pop","ambient","atmospheric","blues","breakbeat","chanson", "chillout","club","dance","darkambient", "darkwave", "deephouse", "disco", "drumnbass", "dubstep", "easylistening", "experimental","folk", "electropop", "funk", "house", "idm", "instrumentalpop", "lounge", "minimal", "newwave", "popfolk", "poprock","rnb","reggae","country", "singersongwriter", "ska", "soul", "synthpop", "techno", "trance" ],
+        "rock": ["alternative", "classicrock", "ethnicrock", "gothic", "grunge", "hardrock", "heavymetal", "indie", "industrial", "instrumentalrock", "metal", "postrock", "progressive", "psychedelic", "punkrock", "rock", "rocknroll"],
+        "ethnic": ["african", "bossanova", "ethnicrock", "ethno", "folk", "latin", "newage", "oriental", "world"],
+        "classic": ["classical", "orchestral", "soundtrack"],        
+        "mongol_country": ["mongolian country"],
+        "mongol_folk": ["mongolian folk"],
+        "kids": ["kids"]
+    }
 
--- services/recommendation_service.py
+++ services/recommendation_service.py
@@ -1,6 +1,7 @@
 from flask import current_app
 from sklearn.metrics.pairwise import cosine_similarity
 from utils.simple_utils import remove_duplicate_items
+import json
 
 
 def get_recommendation_svc(artist_ids, tracks, emotions, genres):
@@ -20,17 +21,19 @@ def get_recommendation_svc(artist_ids, tracks, emotions, genres):
     try:
         tracks_recommendation = get_tracks_recommendation(tracks) 
         recommended_tracks = recommended_tracks + tracks_recommendation
-        
+        # print("track: ", tracks_recommendation[0]["parent_genre_id"])        
         if len(genres) > 0:
             for genre in genres:
-                genre_tracks = get_genre_tracks(genre, 5)                
+                track_genre = current_app.config['GENRE_MAP_BY_NAME'][genre]
+                genre_tracks = get_genre_tracks(track_genre, 200)                                
                 
-                genres_recommendation = get_tracks_recommendation(genre_tracks, 2)
-                recommended_tracks = recommended_tracks + genres_recommendation
+                filtered_genre_tracks = [track for track in genre_tracks if track['parent_genre_name']==track_genre]
+                # print("filtered_genre_tracks: ", genre_tracks[0]['parent_genre_name'])
+                recommended_tracks = recommended_tracks + filtered_genre_tracks                
                 
         if len(emotions) > 0:
             for emotion in emotions:
-                emotion_tracks = get_emotion_tracks(emotion, 5)
+                emotion_tracks = get_emotion_tracks(emotion, 40)
                 
                 emotions_recommendation = get_tracks_recommendation(emotion_tracks, 2)                
                 
@@ -44,7 +47,7 @@ def get_recommendation_svc(artist_ids, tracks, emotions, genres):
         raise Exception(str(e))
 
         
-def get_tracks_recommendation(tracks, recommend_type=1):
+def get_tracks_recommendation(tracks):
     """Function to get recommendation for track list
 
     Args:
@@ -58,14 +61,7 @@ def get_tracks_recommendation(tracks, recommend_type=1):
         for track in tracks:
             result_model_1 = get_recommendation_base_model(track, 40)
             recommended_tracks = recommended_tracks + result_model_1
-            if recommend_type == 1:
-                result_model_2 = get_recommendation_custom_model_01(track, 40)
-                recommended_tracks = recommended_tracks + result_model_2
-            if recommend_type == 1:
-                result_model_3 = get_recommendation_custom_model_02(track, 40)
-                recommended_tracks = recommended_tracks + result_model_3
-            
-            # recommended_tracks = recommended_tracks + result_model_1 + result_model_2 + result_model_3
+
         return recommended_tracks
     except Exception as e:        
         raise Exception(str(e))
@@ -106,15 +102,19 @@ def get_recommendation_base_model(track_m_id, num_recommendations=10):
             track_id = df_tracks.loc[i, 'track_id']
             track_name = str(df_tracks.loc[i, 'track_name'])
             artist_name = str(df_tracks.loc[i, 'artist_name'])
+            parent_gid = df_tracks.loc[i, 'parent_genre_id']
+            parent_name = str(df_tracks.loc[i, 'parent_genre_name'])
 
-            print("{}: {}/{} {} by {} - {}".format(i, track_id, track_m_id, track_name, artist_name, score))
+            # print("{}: {}/{} {} by {} - {}".format(i, track_id, track_m_id, track_name, artist_name, score))
 
             track_info = {
-                "idx": i,
+                # "idx": i,
                 "track_id": int(track_id),
                 "track_m_id": track_m_id,
                 "track_name": track_name,
-                "artist_name": artist_name,
+                "artist_name": artist_name,                
+                "parent_genre_id": int(parent_gid),
+                "parent_genre_name": parent_name,
                 "score": score
             }
             
@@ -210,8 +210,7 @@ def get_recommendation_custom_model_02(track_m_id, num_recommendations=20):
     Returns:
         list: recommended track list
     """
-    try:
-    
+    try:    
         df_tracks_ardiin = current_app.config['DF_TRACKS_ARDIIN']
         model_ardiin = current_app.config['MODEL_ARDIIN']        
         
@@ -264,11 +263,11 @@ def get_recommendation_custom_model_02(track_m_id, num_recommendations=20):
         print({"error": str(e)})
         return []
 
-def get_genre_tracks(genre_name, num_tracks=2):
+def get_genre_tracks(genre, num_tracks=40):
     """Function to retrieve top tracks of given genre
 
     Args:
-        genre_name (string): Give genre name
+        genre (string): Give genre name
         num_tracks (int, optional): limit number of tracks. Defaults to 2.
 
     Returns:
@@ -277,21 +276,35 @@ def get_genre_tracks(genre_name, num_tracks=2):
     genre_tracks = []
     genres = current_app.config['DF_GENRES']
     
-    try:
-        # Filtering out all songs in the Genre column for non-zero or specific values
-        filtered_df = genres[genres[genre_name] != 0]        
+    genres_in_genre = current_app.config['GENRE_MAP'][genre]
+    
+    try:        
         
-        # Sorting the songs of the genre(descending)
-        sorted_df = filtered_df.sort_values(by=genre_name, ascending=False)
-        
-        # Retrieving <num_tracks> tracks from sorted list
-        ordered_genre_tracks = sorted_df[['track_id', genre_name]].values.tolist()
-        ordered_genre_tracks = [[int(track_id), value] for track_id, value in ordered_genre_tracks][:num_tracks]
-
-        # print("Top {} {} songs:".format(num_tracks, genre_name))
-        for track_id, value in ordered_genre_tracks:
-            genre_tracks.append(track_id)
-            # print("track_id: {}, value: {}".format(track_id, value))
+        for genre_name in genres_in_genre:
+            print("genre: ", genre_name)
+            # Filtering out all songs in the Genre column for non-zero or specific values
+            filtered_df = genres[genres[genre_name] != 0]
+            
+            # Shuffling the songs of the genre(descending)
+            shuffled_df = filtered_df.sample(frac=1)
+            # sorted_df = filtered_df.sort_values(by=[genre_name], ascending=False)
+            
+            # Retrieving <num_tracks> tracks from sorted list
+            ordered_genre_tracks = shuffled_df[['artist_name', 'track_id', 'track_m_id', 'track_name', 'parent_genre_id', 'parent_genre_name', genre_name]].values.tolist()        
+            ordered_genre_tracks = [[artist_name, int(track_id), int(track_m_id), track_name, int(parent_genre_id), parent_genre_name, value, ] for artist_name, track_id, track_m_id, track_name, parent_genre_id, parent_genre_name, value in ordered_genre_tracks][:num_tracks]
+                        
+            for artist_name, track_id, track_m_id, track_name, parent_genre_id, parent_genre_name, value in ordered_genre_tracks:
+                track_info = {                
+                    "track_id": track_id,
+                    "track_m_id": track_m_id,
+                    "track_name": track_name,
+                    "artist_name": artist_name,
+                    "parent_genre_id": parent_genre_id,
+                    "parent_genre_name": parent_genre_name,
+                    "score": value
+            }
+                genre_tracks.append(track_info)
+                print("track_id: {}, track_m_id: {}, value: {}, genre: {}".format(track_id, track_m_id, value, parent_genre_name))
     except Exception as e:
         print(e)
     return genre_tracks
@@ -313,17 +326,17 @@ def get_emotion_tracks(emotion_name, num_tracks=2):
         # Filtering out all songs in the Emotion column for non-zero or specific values
         filtered_df = emotions[emotions[emotion_name] != 0]
         
-        # Sorting the songs of the emotion(descending)
-        sorted_df = filtered_df.sort_values(by=emotion_name, ascending=False)
+        # Shuffling the songs of the emotion
+        shuffled_df = filtered_df.sample()
         
         # Retrieving <num_tracks> tracks from sorted list
-        ordered_emotion_tracks = sorted_df[['track_id', emotion_name]].values.tolist()
-        ordered_emotion_tracks = [[int(track_id), value] for track_id, value in ordered_emotion_tracks][:num_tracks]
+        ordered_emotion_tracks = shuffled_df[['track_id', 'track_m_id', emotion_name]].values.tolist()
+        ordered_emotion_tracks = [[int(track_id), int(track_m_id), value] for track_id, track_m_id, value in ordered_emotion_tracks][:num_tracks]
 
         # print("Top {} {} songs:".format(num_tracks, emotion_name)) 
-        for track_id, value in ordered_emotion_tracks:
-            emotion_tracks.append(track_id)
-            # print("track_id: {}, value: {}".format(track_id, value))
+        for track_id, track_m_id, value in ordered_emotion_tracks:
+            emotion_tracks.append(track_m_id)
+            print("track_id: {}, track_m_id: {}, value: {}".format(track_id, track_m_id, value))
     except Exception as e:
-        print(e)
+        print("Emotion track error: ", e)
     return emotion_tracks
